b067d1a4a9f11baccd7d8ed7b7874c8e11db9f48
unknown <imeckard@gmail.com>
2021-09-15 10:57:51 -0400

    Commit to working branch

--- Cinema Room REST Service/task/src/cinema/Controller.java
+++ Cinema Room REST Service/task/src/cinema/Controller.java
@@ -1,8 +1,35 @@
 package cinema;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
 
+    ImportantMessage latestMessage = new ImportantMessage();
+
+    @PostMapping("/message")
+    public void newMessage(@RequestBody ImportantMessage inputMessage) {
+        latestMessage = inputMessage;
+    }
+
+    @GetMapping("/message")
+    public ImportantMessage getLatestMessage(ImportantMessage latestMessage) {
+        return latestMessage;
+    }
+
+
+}
+
+class ImportantMessage {
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String message = "Default Message";
+
+    ImportantMessage() {}
 }
